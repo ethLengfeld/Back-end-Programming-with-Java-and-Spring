396ff8d6c0690376a1d31245d5f3a0497ebe96df
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2024-09-19 18:43:14 -0500

    controller and service test

--- SafetyNet-Alerts/src/test/java/org/safetynet/alerts/controller/AlertsControllerTest.java
+++ SafetyNet-Alerts/src/test/java/org/safetynet/alerts/controller/AlertsControllerTest.java
@@ -1,4 +1,28 @@
 package org.safetynet.alerts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.safetynet.alerts.service.AlertsService;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class AlertsControllerTest {
+
+    private static AlertsService alertsService;
+    private static AlertsController alertsController;
+
+    @BeforeAll
+    static void setUp() {
+        alertsService = mock(AlertsService.class);
+        alertsController = new AlertsController(alertsService);
+    }
+
+    @Test
+    public void testGetPersonsServicedByStationNumber() throws JsonProcessingException {
+        //TODO fix
+        when(alertsService.getStationAddressesFromStationNumber(1)).thenReturn(null);
+        alertsController.getPersonsServicedByStationNumber(1);
+    }
 }
--- SafetyNet-Alerts/src/test/java/org/safetynet/alerts/service/AlertsServiceTest.java
+++ SafetyNet-Alerts/src/test/java/org/safetynet/alerts/service/AlertsServiceTest.java
@@ -1,5 +1,24 @@
 package org.safetynet.alerts.service;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.safetynet.alerts.repository.AlertsRepository;
+
+import static org.mockito.Mockito.mock;
+
 public class AlertsServiceTest {
 
+    private static AlertsRepository alertsRepository;
+    private static AlertsService alertsService;
+
+    @BeforeAll
+    static void setUp() {
+        alertsRepository = mock(AlertsRepository.class);
+        alertsService = new AlertsService(alertsRepository);
+    }
+
+    @Test
+    public void testGetStationAddressesFromStationNumber() {
+
+    }
 }
