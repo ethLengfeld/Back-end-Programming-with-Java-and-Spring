6dbf74cad7035d3712b1b959bba9408d5bb77e15
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2025-01-06 21:59:23 -0600

    index and controller

--- mediscreen/src/main/java/org/openclassrooms/mediscreen/MediscreenApplication.java
+++ mediscreen/src/main/java/org/openclassrooms/mediscreen/MediscreenApplication.java
@@ -2,17 +2,9 @@ package org.openclassrooms.mediscreen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MediscreenApplication {
-
-	@RequestMapping("/")
-	public String home() {
-		return "Welcome to Mediscreen";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MediscreenApplication.class, args);
