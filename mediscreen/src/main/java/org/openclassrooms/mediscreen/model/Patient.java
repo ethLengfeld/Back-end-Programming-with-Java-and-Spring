89e3759ded5c23b4eb7503ca5d1e68840b545dea
Ethan Lengfeld <EthanLengfeld.Dev@gmail.com>
2025-01-15 11:02:45 -0600

    add import

--- mediscreen/src/main/java/org/openclassrooms/mediscreen/model/Patient.java
+++ mediscreen/src/main/java/org/openclassrooms/mediscreen/model/Patient.java
@@ -1,5 +1,6 @@
 package org.openclassrooms.mediscreen.model;
 
+import org.openclassrooms.mediscreen.annotations.PhoneNumber;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
