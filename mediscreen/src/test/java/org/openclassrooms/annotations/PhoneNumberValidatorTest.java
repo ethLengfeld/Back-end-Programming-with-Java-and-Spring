067e7ee9edd597168a32b618d5ce9750deabcf07
EthanLengfeld.Dev@gmail.com <EthanLengfeld.Dev@gmail.com>
2025-01-21 22:17:54 -0600

    PhoneNumberValidator test

--- mediscreen/src/test/java/org/openclassrooms/annotations/PhoneNumberValidatorTest.java
+++ mediscreen/src/test/java/org/openclassrooms/annotations/PhoneNumberValidatorTest.java
@@ -1,5 +1,21 @@
 package org.openclassrooms.annotations;
 
+import com.mongodb.assertions.Assertions;
+import jakarta.validation.ConstraintValidatorContext;
+import org.junit.jupiter.api.Test;
+import org.openclassrooms.mediscreen.annotations.PhoneNumberValidator;
+
+import static org.mockito.Mockito.mock;
+
 public class PhoneNumberValidatorTest {
-    //TODO tests
+
+    @Test
+    void isValid() {
+        PhoneNumberValidator phoneNumberValidator = new PhoneNumberValidator();
+        ConstraintValidatorContext context = mock(ConstraintValidatorContext.class);
+        String validPhoneNumber = "123-456-7891";
+        String invalidPhoneNumber = "21312321132123";
+        Assertions.assertTrue(phoneNumberValidator.isValid(validPhoneNumber, context));
+        Assertions.assertFalse(phoneNumberValidator.isValid(invalidPhoneNumber, context));
+    }
 }
